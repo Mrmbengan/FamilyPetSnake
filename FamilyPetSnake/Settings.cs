4d70d94e5612e1be7d906964c21bf5a20a44a235
matias.bengtsson <matias.bengtsson@ATTR00032692>
2021-08-19 14:27:38 +0200

    Add project files.

---
+++ FamilyPetSnake/Settings.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FamilyPetSnake
+{
+    class Settings
+    {
+
+        public static int Width { get; set; }
+        public static int Height { get; set; }
+
+        public static string directions;
+
+        public Settings() 
+        {
+            Width = 16;
+            Height = 16;
+            directions = "left";
+
+        
+        }
+
+
+
+    }
+}
